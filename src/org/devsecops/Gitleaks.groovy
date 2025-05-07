d254ea64c13ab9e2753c119b9b9c3b97e4f6b1cc
SREENIVASULU RAMANABOINA <112157011+sreeni827@users.noreply.github.com>
2025-05-06 20:36:03 -0500

    Update Gitleaks.groovy

--- src/org/devsecops/Gitleaks.groovy
+++ src/org/devsecops/Gitleaks.groovy
@@ -2,6 +2,14 @@ package org.devsecops
 
 class Gitleaks {
   static void scan() {
-    sh 'docker run --rm -v $(pwd):/repo zricethezav/gitleaks detect --source=/repo'
+    echo "🔐 Starting GitLeaks secrets scan..."
+    
+    sh '''
+      docker run --rm \
+        -v "$(pwd)":/repo \
+        zricethezav/gitleaks detect --source=/repo
+    '''
+
+    echo "✅ GitLeaks scan completed."
   }
 }
