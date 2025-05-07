27f2c72f7223fc61aba318afa9afe52a0983fc22
SREENIVASULU RAMANABOINA <112157011+sreeni827@users.noreply.github.com>
2025-05-07 17:54:21 -0500

    Update Trivy.groovy

--- src/org/devsecops/Trivy.groovy
+++ src/org/devsecops/Trivy.groovy
@@ -1,30 +1,25 @@
 package org.devsecops
 
-def steps
-
-Trivy(steps) {
-  this.steps = steps
-}
-
 class Trivy {
-  def steps
-  Trivy(steps) {
-    this.steps = steps
-  }
+    def steps
 
-  void scan(String image) {
-    steps.echo "🔍 Scanning image with Trivy: ${image}"
-
-    try {
-      steps.sh """
-        docker run --rm \
-          -v /var/run/docker.sock:/var/run/docker.sock \
-          aquasec/trivy image --severity HIGH,CRITICAL ${image}
-      """
-      steps.echo "✅ Trivy scan completed successfully."
-    } catch (Exception e) {
-      steps.echo "❌ Trivy scan failed: ${e.getMessage()}"
-      throw e
+    Trivy(steps) {
+        this.steps = steps
     }
-  }
+
+    void scan(String image) {
+        steps.echo "🔍 Scanning image with Trivy: ${image}"
+
+        try {
+            steps.sh """
+                docker run --rm \
+                  -v /var/run/docker.sock:/var/run/docker.sock \
+                  aquasec/trivy image --severity HIGH,CRITICAL ${image}
+            """
+            steps.echo "✅ Trivy scan completed successfully."
+        } catch (Exception e) {
+            steps.echo "❌ Trivy scan failed: ${e.getMessage()}"
+            throw e
+        }
+    }
 }
