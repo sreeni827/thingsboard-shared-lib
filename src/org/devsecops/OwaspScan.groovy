6b592c26352a1c5a6a67841d1a75b493a3d8b59e
SREENIVASULU RAMANABOINA <112157011+sreeni827@users.noreply.github.com>
2025-05-07 19:58:22 -0500

    Update OwaspScan.groovy

--- src/org/devsecops/OwaspScan.groovy
+++ src/org/devsecops/OwaspScan.groovy
@@ -11,16 +11,17 @@ class OwaspScan {
         steps.echo "🛡️ Starting OWASP Dependency-Check scan..."
 
         steps.sh """
-            mkdir -p owasp-output
-            docker run --rm \
-              -v ${steps.env.WORKSPACE}:/src \
-              -v ${steps.env.WORKSPACE}/owasp-output:/report \
-              owasp/dependency-check \
-              --project "ThingsBoard" \
-              --scan /src \
-              --format "HTML" \
-              --out /report
-        """
+  mkdir -p ${steps.env.WORKSPACE}/owasp-output
+  docker run --rm \
+    -v ${steps.env.WORKSPACE}:/src \
+    -v ${steps.env.WORKSPACE}/owasp-output:/report \
+    -v /tmp/owasp-data:/dependency-check/data \
+    owasp/dependency-check \
+    --project "ThingsBoard" \
+    --scan /src \
+    --format "HTML" \
+    --out /report
+"""
 
         steps.echo "✅ OWASP scan complete. Report saved to: owasp-output/dependency-check-report.html"
     }
