5e4fa3bdedf1eba340b5b4532b238b1785a79ed9
SREENIVASULU RAMANABOINA <112157011+sreeni827@users.noreply.github.com>
2025-05-07 22:24:45 -0500

    Update thingsboardPipeline.groovy

--- vars/thingsboardPipeline.groovy
+++ vars/thingsboardPipeline.groovy
@@ -51,15 +51,15 @@ def call() {
       }
 
       stage('Push Docker Image') {
-        when { expression { return false } }
+        
         steps {
-          echo '⏩ Skipping Docker push for now'
-          // withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-          //   sh '''
-          //     echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-          //     docker push $DOCKER_IMAGE
-          //   '''
-          // }
+         
+                   withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+            sh '''
+              echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+              docker push $DOCKER_IMAGE
+            '''
+           }
         }
       }
     }
