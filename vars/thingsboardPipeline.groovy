5ea78b45cbf577d0ce597839bbe602a5cb8a5117
SREENIVASULU RAMANABOINA <112157011+sreeni827@users.noreply.github.com>
2025-05-18 17:57:47 -0500

    Update thingsboardPipeline.groovy

--- vars/thingsboardPipeline.groovy
+++ vars/thingsboardPipeline.groovy
@@ -91,6 +91,30 @@ def call() {
     }
   }
 }
+    stage('Provision EKS Cluster') {
+  steps {
+    dir('terraform-eks/envs/qa') {
+      script {
+        withCredentials([[
+          $class: 'AmazonWebServicesCredentialsBinding',
+          credentialsId: 'aws-credentials'
+        ]]) {
+          withEnv([
+            'AWS_ACCESS_KEY_ID=' + env.AWS_ACCESS_KEY_ID,
+            'AWS_SECRET_ACCESS_KEY=' + env.AWS_SECRET_ACCESS_KEY,
+            'AWS_DEFAULT_REGION=us-east-1'
+          ]) {
+            sh '''
+              terraform init
+              terraform apply -auto-approve -var-file="terraform.tfvars"
+            '''
+          }
+        }
+      }
+    }
+  }
+}
+
 
 
 
