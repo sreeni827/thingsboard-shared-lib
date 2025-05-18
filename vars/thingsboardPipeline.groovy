a431a802fa99f221b43ef82859a7939d743df125
SREENIVASULU RAMANABOINA <112157011+sreeni827@users.noreply.github.com>
2025-05-18 18:01:27 -0500

    Update thingsboardPipeline.groovy

--- vars/thingsboardPipeline.groovy
+++ vars/thingsboardPipeline.groovy
@@ -4,24 +4,27 @@ def call() {
 
     environment {
       DOCKER_IMAGE = "sreenivasuluramanaboina/thingsboard:kafka-v2"
+      AWS_REGION = "us-east-1"
     }
 
     stages {
+      /*
       stage('Build Backend') {
         when { expression { return false } }
         steps {
-          echo '⏩ Skipping backend build for now'
+          echo '⏩ Skipping backend build'
         }
       }
 
       stage('Build Docker Image') {
         when { expression { return false } }
         steps {
-          echo '⏩ Skipping Docker build for now'
+          echo '⏩ Skipping Docker image build'
         }
       }
 
       stage('Trivy Scan') {
+        when { expression { return false } }
         steps {
           script {
             def trivy = new org.devsecops.Trivy(this)
@@ -31,6 +34,7 @@ def call() {
       }
 
       stage('GitLeaks Scan') {
+        when { expression { return false } }
         steps {
           script {
             def gitleaks = new org.devsecops.Gitleaks(this)
@@ -40,6 +44,7 @@ def call() {
       }
 
       stage('OWASP Scan') {
+        when { expression { return false } }
         steps {
           script {
             def owasp = new org.devsecops.OwaspScan(this)
@@ -49,6 +54,7 @@ def call() {
       }
 
       stage('Push Docker Image') {
+        when { expression { return false } }
         steps {
           withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
             sh '''
@@ -58,6 +64,23 @@ def call() {
           }
         }
       }
+
+      stage('Deploy to EKS with Helm') {
+        when { expression { return false } }
+        steps {
+          script {
+            sh '''
+              aws eks update-kubeconfig --region us-east-1 --name eks-dev
+              helm upgrade --install thingsboard ./helm/thingsboard \
+                --namespace thingsboard \
+                --create-namespace \
+                --set image.repository=sreenivasuluramanaboina/thingsboard \
+                --set image.tag=kafka-v2
+            '''
+          }
+        }
+      }
+      */
 
       stage('Provision EKS Cluster') {
         steps {
@@ -70,7 +93,7 @@ def call() {
                 withEnv([
                   'AWS_ACCESS_KEY_ID=' + env.AWS_ACCESS_KEY_ID,
                   'AWS_SECRET_ACCESS_KEY=' + env.AWS_SECRET_ACCESS_KEY,
-                  'AWS_DEFAULT_REGION=us-east-1'
+                  "AWS_DEFAULT_REGION=${env.AWS_REGION}"
                 ]) {
                   sh '''
                     terraform init
@@ -79,21 +102,6 @@ def call() {
                 }
               }
             }
-          }
-        }
-      }
-
-      stage('Deploy to EKS with Helm') {
-        steps {
-          script {
-            sh '''
-              aws eks update-kubeconfig --region us-east-1 --name eks-dev
-              helm upgrade --install thingsboard ./helm/thingsboard \
-                --namespace thingsboard \
-                --create-namespace \
-                --set image.repository=sreenivasuluramanaboina/thingsboard \
-                --set image.tag=kafka-v2
-            '''
           }
         }
       }
